0dcbecec16af578bf89c3018eddc36eb80fef5ed
samdouble <samleafs@protonmail.com>
2026-03-02 11:06:23 -0500

    feat(moduleA): add very simple project A

---
+++ A/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
