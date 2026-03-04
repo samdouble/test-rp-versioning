1ef5e16eb22c2bf609ace4cc7bd5a84735374566
samdouble <samleafs@protonmail.com>
2026-03-04 15:08:20 -0500

    feat(moduleA)!: introduced a breaking change

--- A/src/main/java/com/example/App.java
+++ A/src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+
 public class App {
     public static void main(String[] args) {
         String version = VersionInfo.getVersion(App.class);
--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,5 +2,6 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': [2, 'always', ['moduleA', 'moduleB', 'core']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test']],
   },
 };
